436affee77bbfcdca5e3e8bc833f464b17684556
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-06 14:09:15 -0600

    Renamed userInputGet to handleFormSubmit. Created new function userContainerHTML. handleFormSubmit now returns userContainerHTML-userInput

--- shopping-challenge/index.js
+++ shopping-challenge/index.js
@@ -1,21 +1,50 @@
 'use strict';
 
-function userInputGet() {
+function handleFormSubmit() {
   $('#js-shopping-list-form').on('submit', function (e) {
     e.preventDefault();
     const userInput = $('.js-shopping-list-entry').val();
     // clear user input after submit
     $('.js-shopping-list-entry').val('');
-    return userInput;
+    return userContainerHTML(userInput);
   });
 }
 
 function main() {
-  userInputGet();
+  handleFormSubmit();
 }
 
 $(main);
 // Create shopping list container 
+/*<ul class="shopping-list">
+      <li>
+        <span class="shopping-item">apples</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>
+*/
+
+function userContainerHTML(input) {
+  const html = `<li>
+  <span class="shopping-item">${input}</span>
+  <div class="shopping-item-controls">
+    <button class="shopping-item-toggle">
+      <span class="button-label">check</span>
+    </button>
+    <button class="shopping-item-delete">
+      <span class="button-label">delete</span>
+    </button>
+  </div>
+</li>`;
+  return html;
+}
+
 // Turn data into HTML and add to shopping list container
 // If user hit check button
 // toggle .shopping-item__checked class
